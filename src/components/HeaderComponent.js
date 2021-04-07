22f42aff19feb726a7e15a23bec5852bb9f3ea7e
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-04-07 19:07:57 +0600

    Added Router and NavBar Links

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,32 +1,64 @@
-import React, {Component} from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import React, {useState} from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import {NavLink} from "react-router-dom";
 
-class Header extends Component {
+const Header = (props) => {
+    const [isOpen, toggleNav] = useState(false);
+    const setToggleNav = () => {return toggleNav(!isOpen);};
+    return (
+        <React.Fragment>
+            <Navbar dark expand="md">
+                <div className="container">
+                    <NavbarToggler onClick={setToggleNav} />
+                    <NavbarBrand className="mr-auto" href="/">
+                        <img src="assets/images/logo.png"
+                             height="30px" width="41px"
+                             alt="Ristorante Con Fusion"
+                        />
+                    </NavbarBrand>
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home">
+                                    <span className="fa fa-home fa-lg"></span> Home
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/about-us">
+                                    <span className="fa fa-info fa-lg"></span> About Us
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/menu">
+                                    <span className="fa fa-list fa-lg"></span> Menu
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/contact-us">
+                                    <span className="fa fa-address-card fa-lg"></span> Contact Us
+                                </NavLink>
+                            </NavItem>
 
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar dark>
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>Ristorante con Fusion</h1>
-                                <p>
-                                    We take inspiration from the World's best cuisines,
-                                    and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!
-                                </p>
-                            </div>
+                        </Nav>
+                    </Collapse>
+                </div>
+            </Navbar>
+            <Jumbotron>
+                <div className="container">
+                    <div className="row row-header">
+                        <div className="col-12 col-sm-6">
+                            <h1>Ristorante con Fusion</h1>
+                            <p>
+                                We take inspiration from the World's best cuisines,
+                                and create a unique fusion experience. Our lipsmacking
+                                creations will tickle your culinary senses!
+                            </p>
                         </div>
                     </div>
-                </Jumbotron>
-            </React.Fragment>
-        );
-    }
+                </div>
+            </Jumbotron>
+        </React.Fragment>
+    );
 }
 
 export default Header;
